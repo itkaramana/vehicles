5387754e3fcce47c00e8eb92926a5e280371e535
aleksandar.karamanov <aleksandar.karamanov@accenture.com>
2021-02-03 22:25:03 +0200

    Adding retryable annotation

--- src/main/java/it/vehicles/interceptor/WebConfiguration.java
+++ src/main/java/it/vehicles/interceptor/WebConfiguration.java
@@ -1,10 +1,12 @@
 package it.vehicles.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableRetry
 public class WebConfiguration implements WebMvcConfigurer {
 
 	@Override
